9147d74f111e8975f5d2076e485582cc0e6ab7a2
Scott Geilen <scottgeilen99@gmail.com>
2020-01-29 21:25:54 -0500

    Looping to other pages is broken

--- index.py
+++ index.py
@@ -1,19 +1,23 @@
 import urllib.request as urllib
-from selenium.webdriver import Chrome
+#from selenium.webdriver import Chrome
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 import re
-
+# initialize the Indeed URL to url string
+pageNumber = 10
+url = 'https://www.indeed.com/jobs?q=software+developer&l=Tucson,+AZ&jt=fulltime&explvl=entry_level'
+#open Chrome and navigate to web page
+driver = webdriver.Chrome()
+driver.get(url)
 # empty array for results
 results = []
-# initialize the Indeed URL to url string
-url = 'https://www.indeed.com/jobs?q=software+developer&l=Tucson,+AZ&jt=fulltime&explvl=entry_level'
 soup = BeautifulSoup(urllib.urlopen(url).read(), 'html.parser')
 # find all the job postings on the page
 results = soup.find_all('div', attrs={'class': 'jobsearch-SerpJobCard'})
 # parse through job postings looking for title, salary, company, rating, location, and summary
-for i in results:        
+for i in results:
         title = i.find('div', attrs={'class':'title'})
         salary = i.find('span', attrs={'class':'salaryText'})
         company = i.find('span', attrs={'class':'company'})
@@ -31,3 +35,8 @@ for i in results:
             print('LOCATION:', location.text.strip())
         if summary is not None:
             print('SUMMARY:', summary.text.strip())
+        pageNumber = pageNumber + 10
+        nextPage = 'https://www.indeed.com/jobs?q=software+developer&l=Tucson%2C+AZ&jt=fulltime&explvl=entry_level&start=' + str(pageNumber)
+        while pageNumber <= 40:
+            driver.get(nextPage)
+driver.close()
